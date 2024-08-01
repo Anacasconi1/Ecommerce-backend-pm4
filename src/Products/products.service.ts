70495e05f1674690c4816e0a5bc75d824430d079
Anacasconi1 <sabriicasconii@gmail.com>
2024-08-01 18:48:45 -0300

    Deploy de aplicacion funcionando

--- src/Products/products.service.ts
+++ src/Products/products.service.ts
@@ -60,7 +60,6 @@ export class ProductsService {
         const catFinder = await this.categoriesRepository.findOne({
           where: { name: prod.category },
         });
-        console.log(catFinder);
         if (!catFinder) {
           throw new NotFoundException(
             'Debes cargar la categoria antes de cargar los productos',
